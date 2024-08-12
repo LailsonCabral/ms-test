2e9bd8a13dd12033295cd32b19971e99f64260f5
Lailson Cabral <lailson.cabral9@gmail.com>
2024-08-12 20:06:15 -0300

    Projeto Base

---
+++ src/main/java/com/lailson/dev/ms_test/MsTestApplication.java
@@ -0,0 +1,13 @@
+package com.lailson.dev.ms_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsTestApplication.class, args);
+	}
+
+}
